ad00c7494224dd8ee298a4fc5fa003736da3cd97
suraj <surajmishra9318@gmail.com>
2023-03-01 17:36:10 +0530

    modifications in css

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-      <div className="previous-operand">1231442.234234</div>
-      <div className="current-operand">1231442.234234</div>
+      <div className="previous-operand"></div>
+      <div className="current-operand"></div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
